53597e8ff27218ba7405fa889310dd824f72d8c4
uniquesonu <21bcs10550@cuchd.in>
2024-04-13 14:05:40 +0530

    Update Course component to use dynamic image URL and alt text

--- app/home/my-cart/page.tsx
+++ app/home/my-cart/page.tsx
@@ -4,11 +4,12 @@ import Total from './Total'
 
 const page = () => {
   return (
-    <div className='w-full container mx-auto min-h-screen'>
+    <div className='w-full container mx-auto mb-16'>
       <h1 className="text-4xl font-bold my-8">Shopping Cart</h1>
         <div className="w-full container mx-auto flex gap-8">
       <div className="w-3/4 grid grid-cols-1 md:grid-cols-1 gap-8">
         <Course
+          imgURL="https://th.bing.com/th/id/OIP.IzKqeXIRSjV-7Rb_bR3k6QHaEK?w=285&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           title="Next.js 14 & React - The Complete Guide"
           author="Maximilian Schwarzmüller"
           rating={4.7}
@@ -18,6 +19,7 @@ const page = () => {
         />
         
         <Course
+          imgURL="https://th.bing.com/th/id/OIP.IzKqeXIRSjV-7Rb_bR3k6QHaEK?w=285&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           title="OpenAI Python API Bootcamp: Learn to use AI, GPT, and more!"
           author="Jose Portilla"
           rating={4.5}
