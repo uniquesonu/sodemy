780d9f335dd1b58758ce15e7e6b55a954d27c101
uniquesonu <21bcs10550@cuchd.in>
2024-04-15 18:16:29 +0530

    Refactor CourseInstructor component to use Next.js Image component for instructor image

--- components/util/Courseinstructor.tsx
+++ components/util/Courseinstructor.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const CourseInstructor = () => {
   return (
-    <div className='flex gap-32'>
+    <div className="flex gap-32">
       <div>Instructor</div>
-      <div className='flex flex-col gap-4'>
-        <div className='flex items-center justify-start gap-8'>
-        <img src="https://img-b.udemycdn.com/user/200_H/4466306_6fd8_3.jpg" className='w-24 h-24 rounded-full' alt="course-instructor" />
-        <div>
-        <h4>Colt Steele</h4>
-        <p>Develop and Bootcamp Instructor</p>  
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-start gap-8">
+          <Image
+            src="https://img-b.udemycdn.com/user/200_H/4466306_6fd8_3.jpg"
+            width={100}
+            height={100}
+            className="w-24 h-24 rounded-full"
+            alt="course-instructor"
+          />
+          <div>
+            <h4>Colt Steele</h4>
+            <p>Develop and Bootcamp Instructor</p>
+          </div>
         </div>
-        </div>
-        <div className='flex gap-8 '>
+        <div className="flex gap-8 ">
           <p>youtube</p>
           <p>website</p>
         </div>
         <div>
-          
-
-Colt Steele
-Developer and Bootcamp Instructor
-
-Hi! I'm Colt. I'm a developer with a serious love for teaching. I've spent the last few years teaching people to program at 2 different immersive bootcamps where I've helped hundreds of people become web developers and change their lives. My graduates work at companies like Google, Salesforce, and Square.
-
-Most recently, I led Galvanize's SF's 6 month immersive program as Lead Instructor and Curriculum Director. After graduating from my class, 94% of my students went on to receive full-time developer roles. I also worked at Udacity as a Senior Course Developer on the web development team where I got to reach thousands of students daily.
+          Colt Steele Developer and Bootcamp Instructor Hi! I&apos;m Colt. I&apos;m a
+          developer with a serious love for teaching. I&apos;ve spent the last few
+          years teaching people to program at 2 different immersive bootcamps
+          where I&apos;ve helped hundreds of people become web developers and change
+          their lives. My graduates work at companies like Google, Salesforce,
+          and Square. Most recently, I led Galvanize&apos;s SF&apos;s 6 month immersive
+          program as Lead Instructor and Curriculum Director. After graduating
+          from my class, 94% of my students went on to receive full-time
+          developer roles. I also worked at Udacity as a Senior Course Developer
+          on the web development team where I got to reach thousands of students
+          daily.
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseInstructor
+export default CourseInstructor;
